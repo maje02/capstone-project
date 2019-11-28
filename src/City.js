1c29ac1b768d3f3bc3798e3347b1494dd62ae6f9
Maje Koenig <maje.koenig@gmail.com>
2019-11-28 17:16:21 +0100

    Sort places by cities

--- src/City.js
+++ src/City.js
@@ -1,8 +1,40 @@
+import React from 'react'
 import styled from 'styled-components/macro'
+import Place from './Place'
+import PropTypes from 'prop-types'
 
-export default styled.section`
+export default function City({ places }) {
+  return (
+    <div>
+      <CitySection city={places[0].city}>--- {places[0].city} ---</CitySection>
+      {renderPlaces()}
+    </div>
+  )
+
+  function renderPlaces() {
+    return places
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map(({ name, street, number, zip_code, city, categories }, index) => (
+        <Place
+          key={index}
+          name={name}
+          street={street}
+          number={number}
+          zip_code={zip_code}
+          city={city}
+          categories={categories}
+        />
+      ))
+  }
+}
+
+const CitySection = styled.section`
   font-family: 'Noto Serif JP';
   color: #b77373;
   text-transform: uppercase;
   font-size: 1.5rem;
 `
+City.propTypes = {
+  places: PropTypes.array.isRequired,
+  city: PropTypes.string.isRequired,
+}
--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,23 @@
 import React from 'react'
-import Place from './Place'
 import places from './places.json'
 import City from './City'
 
 export default function Home() {
-  return (
-    <>
-      {renderCities()}
-      {renderPlaces()}{' '}
-    </>
-  )
+  return <>{renderCities()} </>
 
-  function renderPlaces() {
-    return places
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .sort((a, b) => a.city.localeCompare(b.city))
-      .map(({ name, street, number, zip_code, city, categories }, index) => (
-        <Place
+  function renderCities() {
+    const allCities = places.map(place => place.city)
+    const cities = allCities.filter(
+      (city, index) => allCities.indexOf(city) === index
+    )
+    return cities
+      .sort((a, b) => a.localeCompare(b))
+      .map((city, index) => (
+        <City
           key={index}
-          name={name}
-          street={street}
-          number={number}
-          zip_code={zip_code}
+          places={places.filter(place => place.city === city)}
           city={city}
-          categories={categories}
         />
       ))
-  }
-  function renderCities() {
-    const cities = places.map(item => item.city)
-    const uniqueCities = cities.filter(
-      (city, index) => cities.indexOf(city) === index
-    )
-    //console.log(uniqueCities)
-    return uniqueCities.map((city, index) => (
-      <City key={index} city={city}>
-        --- {city} ---
-      </City>
-    ))
   }
 }
