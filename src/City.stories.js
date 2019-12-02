7fb6c2fdcfead9ee4c7971302e22929b03518617
Maje Koenig <maje.koenig@gmail.com>
2019-12-02 11:59:38 +0100

    Add changes to storybook

--- src/City.stories.js
+++ src/City.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { withKnobs, text } from '@storybook/addon-knobs'
-import { action } from '@storybook/addon-actions'
+import { withKnobs, text, number } from '@storybook/addon-knobs'
+//import { action } from '@storybook/addon-actions'
 import City from './City'
 
 export default {
@@ -10,8 +10,17 @@ export default {
 
 export const standard = () => (
   <City
-    renderPlaces={action('renderPlaces')}
-    city={text('City', 'City Section')}
-    places={['Places', 'Places in City']}
+    places={['Place', 'Place']}
+    name={text('Name', 'Name of Place')}
+    street={text('Street', 'Street of Place')}
+    number={number('Number', 'Number of Place')}
+    zip_code={number('Zip_code', 'Zip_code of Place')}
+    city={text('City', 'City of Place')}
+    phone={number('Phone Number', 'Phone number of Place')}
+    mail={text('Mail', 'E-Mail to Place')}
+    websiteURL={text('Website', 'Website of Place')}
+    opening_times={text('Opening Times', 'Opening Times of Place')}
+    description={text('Description', 'Description of Place')}
+    categories={['Categories', 'Categories of Place']}
   />
 )
