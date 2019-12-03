90f2d8103573c0eb5a3a044a1b1fb468d370b123
Maje Koenig <maje.koenig@gmail.com>
2019-12-03 14:41:32 +0100

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import AppLayout from './AppLayout'
+import Header from './Header'
 import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <AppLayout>
+        <Header>make it stay</Header>
+        <Home />
+      </AppLayout>
     </div>
   )
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+//import React from 'react'
+import styled from 'styled-components/macro'
+
+export default styled.div`
+  background: #637f68;
+  color: #f6d9d3;
+  font-family: 'Noto Serif JP';
+  font-size: 2rem;
+  align-self: center;
+  margin: 0;
+`
