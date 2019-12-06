fc03c8094f5845db626133c86873de062d2344c4
Maje Koenig <maje.koenig@gmail.com>
2019-12-06 15:36:03 +0100

    Set filter on checkboxes

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,29 @@ import FilterMenu from './FilterMenu'
 import City from './City'
 
 export default function Home() {
+  // eslint-disable-next-line no-restricted-globals
+
   const [isClicked, setIsClicked] = useState(false)
-  const [selectedCategory, setSelectedCategory] = useState([])
+  const [categories, setCategories] = useState([
+    { name: 'Bücher', checked: false },
+    { name: 'Elektronik', checked: false },
+    { name: 'Kleidung', checked: false },
+  ])
+  const checkedCategories = categories.map(item =>
+    item.checked ? item.name : ''
+  )
+  console.log(checkedCategories)
+
+  //   const [isSelected, setIsSelected] = useState(false)
   return (
     <HomeWrapper>
       <FilterButton handleClick={() => toggleFilterMenu()}></FilterButton>
       {isClicked ? (
-        <FilterMenu places={places} selectCategory={selectCategory} />
+        <FilterMenu
+          places={places}
+          selectCategory={selectCategory}
+          //category={isSelected}
+        />
       ) : (
         ''
       )}
@@ -25,6 +41,7 @@ export default function Home() {
     const cities = allCities.filter(
       (city, index) => allCities.indexOf(city) === index
     )
+
     return cities
       .sort((a, b) => a.localeCompare(b))
       .map((city, index) => (
@@ -32,17 +49,35 @@ export default function Home() {
           key={index}
           places={places.filter(place => place.city === city)}
           city={city}
-        />
+        >
+          {console.log(
+            places
+              .filter(place => place.city === city)
+              .filter(places =>
+                places.categories.some(arrayEl =>
+                  checkedCategories.some(item => item === arrayEl)
+                )
+              )
+          )}
+        </City>
       ))
   }
   function toggleFilterMenu() {
     setIsClicked(!isClicked)
   }
+
   function selectCategory(category) {
-    setSelectedCategory({
-      ...selectedCategory,
-      [category]: !!selectedCategory,
-    })
+    const index = categories.findIndex(element => element.name === category)
+    console.log(index)
+
+    setCategories([
+      ...categories.slice(0, index),
+      {
+        ...categories[index],
+        checked: !categories[index].checked,
+      },
+      ...categories.slice(index + 1),
+    ])
   }
 }
 
