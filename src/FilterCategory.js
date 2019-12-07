de6e397bae75456e2a623d2cd6dc0ccbab217200
Maje Koenig <maje.koenig@gmail.com>
2019-12-07 23:16:55 +0100

    Add changes with bugs

--- src/FilterCategory.js
+++ src/FilterCategory.js
@@ -10,19 +10,42 @@ export default function FilterCategory(
     <FilterCategoryStyled key={index}>
       <FilterCheckbox
         type="checkbox"
-        onClick={() => selectCategory(category)}
+        onChange={() => selectCategory(category)}
         checked={checked(category)}
-      />{' '}
+      />
       {category}
     </FilterCategoryStyled>
   )
 }
+
 const FilterCategoryStyled = styled.label`
   font-family: 'Noto Serif JP';
   font-size: 0.8rem;
-  display: block;
+  display: inline-flex;
+  cursor: pointer;
+  position: relative;
 `
 const FilterCheckbox = styled.input`
+  /* height: 12px;
+  width: 12px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -o-appearance: none;
+  appearance: none;
+  border: 1px solid #6c6c63;
   border-radius: 0;
-  background: hotpink;
+  outline: none;
+  background-color: hotpink;
+   ${props => (props.checked ? '#b77373' : 'white')}; 
+  cursor: pointer;
+
+    "&::before": {
+    content: '\2713';
+    display: block;
+    text-align: center;
+    color: #41b883;
+    position: absolute;
+    left: 0.7rem;
+    top: 0.2rem;
+  } */ 
 `
