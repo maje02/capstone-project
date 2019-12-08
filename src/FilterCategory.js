deddbe7593a2ad647e1e472cfc890e78f3aa1480
Maje Koenig <maje.koenig@gmail.com>
2019-12-08 11:21:45 +0100

    Delete console logs

--- src/FilterCategory.js
+++ src/FilterCategory.js
@@ -8,7 +8,7 @@ export default function FilterCategory(
 ) {
   return (
     <FilterCategoryStyled key={index}>
-      <FilterCheckbox
+      <input
         type="checkbox"
         onChange={() => selectCategory(category)}
         checked={checked(category)}
@@ -24,28 +24,4 @@ const FilterCategoryStyled = styled.label`
   display: inline-flex;
   cursor: pointer;
   position: relative;
-`
-const FilterCheckbox = styled.input`
-  /* height: 12px;
-  width: 12px;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  -o-appearance: none;
-  appearance: none;
-  border: 1px solid #6c6c63;
-  border-radius: 0;
-  outline: none;
-  background-color: hotpink;
-   ${props => (props.checked ? '#b77373' : 'white')}; 
-  cursor: pointer;
-
-    "&::before": {
-    content: '\2713';
-    display: block;
-    text-align: center;
-    color: #41b883;
-    position: absolute;
-    left: 0.7rem;
-    top: 0.2rem;
-  } */ 
 `
